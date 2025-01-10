8747e420fdfc74dc514a862b7f8cdcac4a78c401
carmenrdr <carmenrumi@gmail.com>
2025-01-10 18:10:53 +0100

    Nuevo guardado

--- src/main/java/org/iesalandalus/programacion/matriculacion/dominio/Curso.java
+++ src/main/java/org/iesalandalus/programacion/matriculacion/dominio/Curso.java
@@ -1,4 +1,26 @@
 package org.iesalandalus.programacion.matriculacion.dominio;
 
 public enum Curso {
+
+    PRIMERO("Primero"), SEGUNDO("Segundo");
+    private String cadenaAMostrar;
+
+    private Curso (String cadenaAMostrar) {
+        this.cadenaAMostrar = cadenaAMostrar;
+    }
+
+    public String imprimir() {
+        if (this == PRIMERO) {
+            return "0.-"+this.cadenaAMostrar;
+        } else if (this == SEGUNDO) {
+            return "1.-"+this.cadenaAMostrar;
+        } else {
+            throw new IllegalArgumentException("El nombre del curso no es válido.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.cadenaAMostrar;
+    }
 }
