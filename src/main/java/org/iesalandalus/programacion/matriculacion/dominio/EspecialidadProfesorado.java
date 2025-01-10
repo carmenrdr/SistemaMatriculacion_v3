7860f41af62c6a71f7f6c542f32706b884f9a6d1
carmenrdr <carmenrumi@gmail.com>
2025-01-10 19:46:09 +0100

    Nuevo guardado

---
+++ src/main/java/org/iesalandalus/programacion/matriculacion/dominio/EspecialidadProfesorado.java
@@ -0,0 +1,30 @@
+package org.iesalandalus.programacion.matriculacion.dominio;
+
+public enum EspecialidadProfesorado {
+
+    INFORMATICA("Informática"),
+    SISTEMAS("Sistemas y Aplicaciones informáticas"),
+    FOL("Formación y Orientación Laboral");
+    private String cadenaAMostrar;
+
+    private EspecialidadProfesorado(String cadenaAMostrar){
+        this.cadenaAMostrar = cadenaAMostrar;
+    }
+
+    public String imprimir(){
+        if (this == INFORMATICA) {
+            return "0.-"+this.cadenaAMostrar;
+        } else if (this == SISTEMAS) {
+            return "1.-"+this.cadenaAMostrar;
+        } else if (this == FOL) {
+            return "2.-"+this.cadenaAMostrar;
+        } else {
+            throw new IllegalArgumentException("La especialidad del profesorado no es válida.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.cadenaAMostrar;
+    }
+}
