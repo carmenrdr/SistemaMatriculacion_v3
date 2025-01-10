6bc43cd76b69b3f53a16494d5250498d363571bc
carmenrdr <carmenrumi@gmail.com>
2025-01-10 18:23:53 +0100

    Nuevo guardado

--- src/main/java/org/iesalandalus/programacion/matriculacion/dominio/Grado.java
+++ src/main/java/org/iesalandalus/programacion/matriculacion/dominio/Grado.java
@@ -1,4 +1,28 @@
 package org.iesalandalus.programacion.matriculacion.dominio;
 
 public enum Grado {
+
+    GDCFGB, GDCFGM, GDCFGS;
+    private String cadenaAMostrar;
+
+    private Grado(String cadenaAMostrar) {
+        this.cadenaAMostrar = cadenaAMostrar;
+    }
+
+    public String imprimir(){
+        if (this == GDCFGB) {
+            return "0.-"+this.cadenaAMostrar;
+        } else if (this == GDCFGM) {
+            return "1.-"+this.cadenaAMostrar;
+        } else if (this == GDCFGS) {
+            return "2.-"+this.cadenaAMostrar;
+        } else {
+            throw new IllegalArgumentException("El grado del ciclo formativo no es válido.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.cadenaAMostrar;
+    }
 }
