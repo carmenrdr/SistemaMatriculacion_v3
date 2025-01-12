ffb771af14ad7ff656547fb327f330c72c54ca5f
carmenrdr <carmenrumi@gmail.com>
2025-01-12 18:58:32 +0100

    Nuevo guardado

--- src/main/java/org/iesalandalus/programacion/matriculacion/vista/Consola.java
+++ src/main/java/org/iesalandalus/programacion/matriculacion/vista/Consola.java
@@ -1,6 +1,8 @@
 package org.iesalandalus.programacion.matriculacion.vista;
 
 import org.iesalandalus.programacion.matriculacion.dominio.*;
+import org.iesalandalus.programacion.matriculacion.negocio.Alumnos;
+import org.iesalandalus.programacion.matriculacion.negocio.Asignaturas;
 import org.iesalandalus.programacion.matriculacion.negocio.CiclosFormativos;
 import org.iesalandalus.programacion.utilidades.Entrada;
 
@@ -60,13 +62,9 @@ public class Consola {
         }
     }
 
-    public Asignatura leerAsignatura(){
-
-    }
-
     public Alumno getAlumnoPorDni(){
         try {
-            System.out.println("Introduzca el DNI del alumno/a a buscar: ");
+            System.out.println("Introduzca el DNI del alumno/a: ");
             String dniABuscar = Entrada.cadena();
 
             Alumno alumnoInventado = new Alumno("Filemón", dniABuscar, "busca@busca.com", "000000000", LocalDate.of(1996, 1, 1));
@@ -145,20 +143,20 @@ public class Consola {
             System.out.println("No hay Ciclos Formativos registrados.");
         } else {
             System.out.println("Los Ciclos Formativos registrados son los siguientes: ");
-            CicloFormativo[] ciclosRegistrados = ciclosFormativos.get();
 
-            for (int i=0; i< ciclosRegistrados.length; i++){
-                System.out.println(ciclosRegistrados[i].imprimir());
+            for (int i=0; i< ciclosFormativos.getTamano(); i++){
+                CicloFormativo cicloFormativo = ciclosFormativos.get()[i];
+                System.out.println(cicloFormativo);
             }
         }
     }
 
     public CicloFormativo getCicloFormativoPorCodigo(){
         try {
-            System.out.println("Introduzca el código del Ciclo Formativo que desea buscar: ");
+            System.out.println("Introduzca el código del Ciclo Formativo: ");
             int codigoBuscar = Entrada.entero();
 
-            CicloFormativo cicloInventado = new CicloFormativo(codigoBuscar, "Programacion", Grado.valueOf("GDCFGB"), "DAM", 100);
+            CicloFormativo cicloInventado = new CicloFormativo(codigoBuscar, "Programacion", Grado.GDCFGB, "DAM", 100);
             return cicloInventado;
 
         } catch (IllegalArgumentException e){
@@ -210,6 +208,84 @@ public class Consola {
         } while (opcion < 0 || opcion > 2);
 
         return EspecialidadProfesorado.values()[opcion];
+    }
+
+    public Asignatura leerAsignatura(CicloFormativo cicloFormativo){
+        System.out.println("Introduzca el código de la asignatura: ");
+        String codigo = Entrada.cadena();
+
+        System.out.println("Introduzca el nombre: ");
+        String nombre = Entrada.cadena();
+
+        System.out.println("Introduzca las horas anuales: ");
+        int horasAnuales = Entrada.entero();
+
+        Curso curso = leerCurso();
+
+        System.out.println("Introduzca las horas de desdoble: ");
+        int horasDesdoble = Entrada.entero();
+
+        EspecialidadProfesorado especialidadProfesorado = leerEspecialidadProfesorado();
+
+        try {
+            return new Asignatura(codigo, nombre, horasAnuales, curso, horasDesdoble, especialidadProfesorado, cicloFormativo);
+        } catch (IllegalArgumentException e){
+            throw new IllegalArgumentException("ERROR: La asignatura introducida no es correcta.\n"+e.getMessage());
+        }
+    }
+
+    public Asignatura getAsignaturaPorCodigo(){
+        try {
+            System.out.println("Introduzca el código de la asignatura: ");
+            String codigoBuscar = Entrada.cadena();
+
+            CicloFormativo cicloInventado = new CicloFormativo(3333, "Programacion", Grado.GDCFGB, "DAM", 100);
+
+            Asignatura asignaturaInventada = new Asignatura(codigoBuscar, "Programación", 10, Curso.PRIMERO, 10, EspecialidadProfesorado.FOL,cicloInventado);
+            return asignaturaInventada;
+
+        } catch (IllegalArgumentException e){
+            throw new IllegalArgumentException("ERROR: "+e.getMessage());
+        }
+    }
+
+    private void mostrarAsignaturas(Asignaturas asignaturas){
+        if (asignaturas == null || asignaturas.getTamano()==0){
+            System.out.println("No hay asignaturas registradas.");
+        } else {
+            System.out.println("Las asignaturas registradas son las siguientes: ");
+
+            for (int i = 0; i < asignaturas.getTamano(); i++) {
+                Asignatura asignatura = asignaturas.get()[i];
+                System.out.println(asignatura);
+            }
+        }
+    }
+
+    private boolean asignaturaYaMatriculada(Asignatura[] asignaturasMatricula, Asignatura asignatura){
+        if (asignaturasMatricula == null || asignatura == null){
+            return false;
+        }
+
+        for (int i=0; i < asignaturasMatricula.length; i++){
+            if (asignaturasMatricula[i] != null && asignaturasMatricula[i].equals(asignatura)){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public Matricula leerMatricula(Alumnos alumnos, Asignaturas asignaturas){
+        System.out.println("Introduce el ID de la mátricula: ");
+        int id = Entrada.entero();
+
+        Curso curso = leerCurso();
+
+        System.out.println("Introduzca la fecha matriculación: ");
+        LocalDate fechaMatricula = LocalDate.parse(Entrada.cadena());
+
+        Alumno alumno
     }
 
 }
