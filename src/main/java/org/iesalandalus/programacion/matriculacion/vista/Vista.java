095869673da126db45bedccc43d6d0fb54682520
carmenrdr <carmenrumi@gmail.com>
2025-02-10 10:15:44 +0100

    Nuevo guardado.

---
+++ src/main/java/org/iesalandalus/programacion/matriculacion/vista/Vista.java
@@ -0,0 +1,8 @@
+package org.iesalandalus.programacion.matriculacion.vista;
+
+import org.iesalandalus.programacion.matriculacion.controlador.Controlador;
+
+public class Vista {
+
+    private Controlador controlador;
+}
